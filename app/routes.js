32ad967229666b0d476672c0866ba52076d054c6
Guy Tristram <guy.tristram@digital.trade.gov.uk>
2020-03-13 16:53:19 +0000

    logic added to simplify data and pages

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,11 @@ router.use('/', require('./routes/questions'));
 router.get('/', function (req, res) {
 	req.session.destroy();
 	res.render('index');
+})
+
+router.get('/prototype-admin/view-the-data', function(req, res)
+{
+	res.render('prototype-admin/view-data', { data: JSON.stringify( req.session, null, 2) } )
 });
 
 module.exports = router;
